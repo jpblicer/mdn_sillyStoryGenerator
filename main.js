132e84724f8bd876ba671b3e0c9f1b4e44e40f99
jpblicer <jpblicer@gmail.com>
2022-05-29 10:57:29 +0900

    Added xyzItem variables, name replacement function, added weight and temperature conversion. NEEDS FIX returning NaN

--- main.js
+++ main.js
@@ -8,6 +8,9 @@ function randomValueFromArray(array){
 }
 
 
+
+
+
 const storyText= 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.'
 
 const insertX=['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
@@ -16,3 +19,37 @@ const insertY=['the soup kitchen', 'Disneyland', 'the White House'];
 
 const insertZ=['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
+
+
+
+
+
+randomize.addEventListener('click', result);
+
+function result() {
+
+  let newStory=storyText;
+
+  let xItem = randomValueFromArray(insertX);
+  let yItem = randomValueFromArray(insertY);
+  let zItem = randomValueFromArray(insertZ);
+
+  newStory=newStory.replace(':insertx:', randomValueFromArray(insertX));
+  newStory=newStory.replace(':inserty:', randomValueFromArray(insertY));
+  newStory=newStory.replace(':insertz:', randomValueFromArray(insertZ));
+
+
+  if(customName.value !== '') {
+    const name = customName.value;
+    newStory=newStory.replace('Bob', name);
+  }
+
+  if(document.getElementById("uk").checked) {
+    const weight = Math.round((300/14) + ' stone');
+    const temperature =  Math.round((94-32)*(5/9) + ' centigrade');
+    newStory=newStory.replace('94 fahrenheit', temperature);
+    newStory=newStory.replace('300 pounds', weight);
+  }
+  story.textContent = newStory;
+  story.style.visibility = 'visible';
+}
